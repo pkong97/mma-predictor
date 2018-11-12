789c7489cbd96f04cb0f292ade1c6f8302c00b1a
pkong97 <kongpatrick97@gmail.com>
2018-11-12 09:14:03 -0800

    in progress

--- webscraper-fightmetric.py
+++ webscraper-fightmetric.py
@@ -3,11 +3,14 @@ from bs4 import BeautifulSoup as soup
 
 my_url = 'http://fightmetric.com/statistics/fighters?char='
 
-uClient = uReq(my_url)
-page_html = uClient.read()
+def fightmetric_scraper(url, filename)
+	uClient = uReq(url)
+	page_html = uClient.read()
 
-page_soup = soup(page_html, "html.parser")
+	page_soup = soup(page_html, 'html.parser')
 
-for link in page_soup.findAll('a', attrs={'href':re.compile("^http://fightmetric.com/fighter-details")}):
-	current_fighter = link.get('href')
-	
+	for link in page_soup.findAll('a', attrs={'href':re.compile("^http://fightmetric.com/fighter-details")}):
+		current_fighter = link.get('href')
+		fighter_page = uReq(url).read()
+		fighter_soup = soup(fighter_page, 'html.parser')
+		
