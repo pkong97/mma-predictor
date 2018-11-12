99dec4a9897a48892b01546cf507d2418b0f1c05
pkong97 <kongpatrick97@gmail.com>
2018-11-11 17:03:29 -0800

    added fighter links

--- webscraper-fightmetric.py
+++ webscraper-fightmetric.py
@@ -1,13 +1,13 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
-my_url = 'http://www.fightmetric.com/fighter-details/634bb0de2eb043b4'
-
-my_url2 = 'http://www.ufc.com/event/UFC-222'
+my_url = 'http://fightmetric.com/statistics/fighters?char='
 
 uClient = uReq(my_url)
 page_html = uClient.read()
 
 page_soup = soup(page_html, "html.parser")
 
-career_stats = page_soup.findAll("div", {"class":"b-list__info-box-left"}) 
+for link in page_soup.findAll('a', attrs={'href':re.compile("^http://fightmetric.com/fighter-details")}):
+	current_fighter = link.get('href')
+	
