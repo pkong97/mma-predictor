719e164dce57197832b079c0b6aa68f391a1dc68
pkong97 <kongpatrick97@gmail.com>
2018-11-18 21:51:34 -0800

    scraping fights

--- webscrapers/webscraper-fights.py
+++ webscrapers/webscraper-fights.py
@@ -5,13 +5,37 @@ import string
 import re
 
 my_url = 'http://fightmetric.com/statistics/events/completed?page=all'
-headers = ''
+headers = 'event_id,event_name,date,f1_id,f1_name,f2_id,f2_name,winner_id'
 
 def fight_scraper(url, filename):
 	file = open(filename, "w")
+	page_soup = soup(uReq(url).read(), 'html.parser')
 	events = page_soup.findAll('a', attrs={'href':re.compile("^http://fightmetric.com/event-details")})
 
 	for link in events:
 		current_event = link.get('href')
-		page_soup = soup(uReq(current_event).read(), 'html.parser')
-		
+		try:
+			event_page = uReq(current_event).read()
+			event_soup = soup(event_page, 'html.parser')
+		except HTTPError as e:
+				e.read()
+		event_id = current_event.split('/')[-1]
+		event_name = event_soup.findAll('span',{'class':'b-content__title-highlight'})[0].text.strip()
+		date = event_soup.findAll("li", {"class":"b-list__box-list-item"})[0].text.strip("[Date: \n]")
+
+		fighters = event_soup.findAll('a', attrs={'href':re.compile("^http://fightmetric.com/fighter-details")})
+
+		for i in range(0, len(fighters), 2):
+			f1_id = fighters[i].get('href').split("/")[-1]
+			f1_name = fighters[i].text.strip()
+			f2_id = fighters[i+1].get('href').split("/")[-1]
+			f2_name = fighters[i+1].text.strip()
+			winner_id = f1_id
+
+			file.write(event_id + ',' + event_name + ',' + date + ',' + f1_id + ',' + f1_name
+				+ ',' + f2_id + ',' + f2_name + ',' + winner_id)
+			print(f1_name + " vs. " f2_name)
+
+	file.close()
+
+fight_scraper(my_url, "fight-database.csv")
---
+++ webscrapers/webscraper-fights2.py
@@ -0,0 +1,38 @@
+from urllib.request import urlopen as uReq
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup as soup
+import string
+import re
+
+my_url = 'http://fightmetric.com/statistics/events/completed?page=all'
+headers = 'event_id, method'
+movs = ['KO', 'SUB', 'DEC', 'Overturned']
+
+def fight_scraper(url, filename):
+	file = open(filename, "w")
+	page_soup = soup(uReq(url).read(), 'html.parser')
+	events = page_soup.findAll('a', attrs={'href':re.compile("^http://fightmetric.com/event-details")})
+
+	for link in events:
+		current_event = link.get('href')
+		event_id = current_event.split('/')[-1]
+		try:
+			event_page = uReq(current_event).read()
+			event_soup = soup(event_page, 'html.parser')
+		except HTTPError as e:
+				e.read()
+		
+		methods = event_soup.findAll("p", {"class":"b-fight-details__table-text"})
+
+		for i in range(0, len(methods)):
+			if any(x in methods[i].text.strip() for x in movs):
+           		method = methods[i].text.strip()
+           	elif ":" in methods[i].text.strip():
+           		time = methods[i].text.strip()
+           	
+           	file.write(event_id + ',' + method + ',' + end_round + ',' + time)
+           	print(method)
+
+    file.close()
+
+fight_scraper(my_url, "fight-database2.csv")
